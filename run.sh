990e3df7e1f30fba3fbd0acdd6b64525fb44776e
Phathdt <phathdt379@gmail.com>
2023-10-23 11:04:21 +0700

    add ELAPSED_TIME

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+SECONDS=0
 pg_dump -Z0 -j 10 -Fd $DB_NAME -U $DB_USERNAME -h $DB_HOST -p $DB_PORT -f dumpdir
 tar -cf - dumpdir | pigz -p 10 > databasebackup.tar.gz
 rm -rf dumpdir
@@ -9,6 +10,8 @@ CURRENT_TIME=$(date +'%Y.%m.%d.%H.%M.%S')
 aws s3 cp ./databasebackup.tar.gz s3://$AWS_BUCKET/$AWS_PATH/$CURRENT_TIME/backup.tar.gz
 
 rm ./databasebackup.tar.gz
+
+ELAPSED_TIME="$((SECONDS/3600))h $(((SECONDS/60)%60))m $((SECONDS%60))s"
 
 curl -X POST -H 'Content-type: application/json' --data "{
   \"text\": \"[Backup::Success] $DB_NAME to $AWS_PATH S3\",
@@ -20,6 +23,11 @@ curl -X POST -H 'Content-type: application/json' --data "{
           \"value\": \"$AWS_BUCKET/$AWS_PATH/$CURRENT_TIME/backup.tar.gz\",
           \"short\": false
         },
+        {
+          \"title\": \"Elapsed Time\",
+          \"value\": \"$ELAPSED_TIME\",
+          \"short\": false
+        }
       ]
     }
   ]
